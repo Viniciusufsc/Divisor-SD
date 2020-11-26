6e0710a1fb4472acc321e8fa09adec01794d79f7
Gmk <44400533+Egamik@users.noreply.github.com>
2020-11-26 09:51:12 -0300

    Update comparadorMSBlsb.vhd

--- comparadorMSBlsb.vhd
+++ comparadorMSBlsb.vhd
@@ -12,11 +12,11 @@ end entity;
 architecture behav of comparadorMSBlsb is
 
 signal tmp: std_logic_vector(n-2 downto 1);
-
+signal tmp1: std_logic;
 begin
 -- nand de 1 ate n-2
 gen: for i in 1 to n-3 generate
-	tmp(i) <= tmp(i+1) nand tmp(i);
+	tmp1 <= tmp(i+1) nand tmp(i);
 end generate;
-saida <= tmp and entrada(n-1) and entrada(0);	
+saida <= tmp1 and entrada(n-1) and entrada(0);	
 end behav;
